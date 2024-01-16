2047478f76932af60a3519442ab5e8ef1e2cb1e5
gabriel <vnvsgabe@gmail.com>
2024-01-16 00:10:13 -0300

    criado CartMenu

---
+++ src/components/button/cart-button/CartButton.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { BsCart4 } from 'react-icons/bs'
+
+import styles from './CartButton.module.css'
+import CartMenu from '@/components/cart-menu/CartMenu'
+
+export default function CartButton() {
+  const [open, setOpen] = useState(false)
+  return (
+    <div>
+      <BsCart4
+        className={styles.icon}
+        size={30}
+        onClick={() => {
+          setOpen(!open)
+        }}
+      />
+      {open && <CartMenu />}
+    </div>
+  )
+}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,7 @@
-import { BsCart4 } from 'react-icons/bs'
-
 import styles from './Navbar.module.css'
 import Logo from '../logo/Logo'
 import Input from '../input/Input'
+import CartButton from '../button/cart-button/CartButton'
 
 export default function Navbar() {
   return (
@@ -11,7 +10,7 @@ export default function Navbar() {
       <div className={styles.search}>
         <Input type="text" placeholder="Buscar" fullWidth />
       </div>
-      <BsCart4 size="40" />
+      <CartButton />
     </nav>
   )
 }
